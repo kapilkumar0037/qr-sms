5740601f165a0393dee5810dffdab0ba2777f454
Kapil Kumar <kapilkumar0037@gmail.com>
2025-01-11 18:03:41 +0530

    send sms using qr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QRCodeComponent } from 'angularx-qrcode';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QRCodeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'qr-sms';
+  countdownTimer = 120;
+
+  ngOnInit() {
+    timer(0, 1000).subscribe(() => {
+      this.countdownTimer = this.countdownTimer - 1;
+    })
+  }
 }
